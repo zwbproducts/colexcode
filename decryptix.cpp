8e5449fb21f96b8ea03f0d7afad3d21d89b41dad
zwbproducts <zwbproducts@live.com>
2017-08-23 13:11:35 +0200

    Update decryptix.cpp
    Code using basic fstream to open and read a file.

--- decryptix.cpp
+++ decryptix.cpp
@@ -1,8 +1,31 @@
 #include "DefinedValues.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 218: #include "Game.h"
 219:
 220: int main()
 221: {
+     std::string filename = "test.bin";
+  std::fstream s(filename, s.binary | s.trunc | s.in | s.out);
+  if (!s.is_open()) {
+    std::cout << "failed to open " << filename << '\n';
+  } else {
+    // write
+    double d = 3.14;
+    s.write(reinterpret_cast<char*>(&d), sizeof d); // binary output
+    s << 123 << "abc";                              // text output
+ 
+    // for fstream, this moves the file position pointer (both put and get)
+    s.seekp(0);
+ 
+    // read
+    s.read(reinterpret_cast<char*>(&d), sizeof d); // binary input
+    int n;
+    std::string str;
+    if (s >> n >> str)                             // text input
+      std::cout << "read back from file: " << d << ' ' << n << ' ' << str << '\n';
+  }
 222: cout << "Decryptix. Copyright 1999 Liberty ";
 223: cout << "Associates, Inc. Version 0.3\n\n" << endl;
 224: bool playAgain = true;
