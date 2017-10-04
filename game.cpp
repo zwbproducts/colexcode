e194dbef6fe88aeded4471be37eea21e701d5931
zwbproducts <zwbproducts@live.com>
2017-10-04 15:28:46 +0200

    Update game.cpp
    added stl functionality to added random values to a container with rand() function.

--- game.cpp
+++ game.cpp
@@ -120,6 +120,26 @@ howManyLetters )
          std::mismatch(inn.begin(), inn.end(), inn.rbegin()).first);
     
      }
+    std::vector<int> v(5);
+    std::generate(v.begin(), v.end(), std::rand); // Using the C function rand()
+ 
+    std::cout << "v: ";
+    for (auto iv: v) {
+        std::cout << iv << " ";
+    }
+    std::cout << "\n";
+ 
+    // Initialize with default values 0,1,2,3,4 from a lambda function
+    // Equivalent to std::iota(v.begin(), v.end(), 0);
+    int n = {0};
+    std::generate(v.begin(), v.end(), [&n]{ return n++; });
+ 
+    std::cout << "v: ";
+    for (auto iv: v) {
+        std::cout << iv << " ";
+    }
+    std::cout << "\n";
+}
 143: srand( (unsigned)time( NULL ) );
 144: 
 145: //for ( i = 0; i < howManyPositions; )
